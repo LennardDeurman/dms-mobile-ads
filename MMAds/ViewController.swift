c97463fa61c4d624672029ffe9573080be4e7a07
deurman <lennarddeurman@live.nl>
2018-09-08 15:55:47 +0200

    Initial Commit

---
+++ MMAds/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MMAds
+//
+//  Created by L.D. Deurman on 08/09/2018.
+//  Copyright © 2018 deurman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
